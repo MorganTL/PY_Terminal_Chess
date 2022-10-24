cee79db4c88008e161ff2dd23f878a904e18f951
MorgenTL <60503995+MorgenTL@users.noreply.github.com>
2022-10-24 12:07:03 +0800

    Added game_board caching in move_pieces function Added undo gameboard  function Added is_checkmate checking function

--- chess.py
+++ chess.py
@@ -1,3 +1,4 @@
+from functools import cache
 import click
 
 # click docs : https://click.palletsprojects.com/en/8.1.x/utils/
@@ -5,14 +6,18 @@ import click
 
 class game_board():
 
-    def __init__(self, size: int):
+    def __init__(self, size: int, cache_size: int = 6):
         self.size = size
         self.game_board = self.make_board(size)
+
+        self.game_board_cache = [] # Does not store current game_board
+        self.cache_size = cache_size
+
         self.pieces_init_pos = {}
         self.player_moves = []
         self.killed_pieces = []
 
-    
+
     def make_board(self, size):
         """
         Generate size x size gameboard dictionary
@@ -40,7 +45,7 @@ class game_board():
             self.pieces_init_pos[k] = pieces[k][:]
             for pos in pieces[k]:
                 self.game_board[pos] = k
-        
+
         return True
 
     def print_board(self):
@@ -50,12 +55,12 @@ class game_board():
         return 0
         """
         alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        
+
         alpha_st = " ".join(alphas[:self.size])
         click.secho(alpha_st.center(self.size*3), fg="bright_white")
         click.echo()
 
-        for i in range(self.size, 0, -1):            
+        for i in range(self.size, 0, -1):
 
             click.secho(f'{i}'.ljust(4), fg="bright_white", nl = False)
 
@@ -64,7 +69,7 @@ class game_board():
                 if (self.game_board[(alphas[j])+str(i)]) == "·":
                     click.secho(f'{self.game_board[(alphas[j])+str(i)]} ', fg='green', nl = False)
                 elif len(self.game_board[(alphas[j])+str(i)]) == 1:
-                    click.secho(f'{self.game_board[(alphas[j])+str(i)]} ', fg='bright_white', nl = False)                
+                    click.secho(f'{self.game_board[(alphas[j])+str(i)]} ', fg='bright_white', nl = False)
                 elif (self.game_board[(alphas[j])+str(i)][0]) == "R":
                     click.secho(f'{self.game_board[(alphas[j])+str(i)][1]} ', fg='bright_red', nl = False)
                 elif (self.game_board[(alphas[j])+str(i)][0]) == "B":
@@ -75,12 +80,12 @@ class game_board():
         click.echo()
         click.secho(alpha_st.center(self.size*3), fg="bright_white")
         return 0
-    
-    
+
+
     def move_piece(self, pos:str, to:str):
         """
         !!Dangerous!! This function does not provide movement checking\n
-        Teleport piece on "pos" -> "to"       
+        Teleport piece on "pos" -> "to" and store perious board into game_board_cache
         pos: string (e.g. "A1")
         to: string (e.g. "B1")
 
@@ -90,7 +95,13 @@ class game_board():
         piece = self.game_board[pos]
         if piece == "·":
             return False
-        
+
+        self.game_board_cache += [self.game_board.copy()]
+
+        if len(self.game_board_cache) > self.cache_size:
+            self.game_board_cache.pop(0)
+
+
         self.game_board[to] = piece
         self.game_board[pos] = "·"
         return True
@@ -99,14 +110,14 @@ class game_board():
         pass
 
     def move_generator(self, pos: str):
-        """        
+        """
         pos: string (e.g. "A1")\n
         return a list of legal moves that the pieces can do
         """
         if (self.game_board[pos]) == "·":
             return []
         piece = self.game_board[pos][1]
-        
+
         # click.secho(f"At {pos}, there is {piece}", fg="green")
 
         if piece == "R":
@@ -114,10 +125,10 @@ class game_board():
 
         elif piece == "B":
             return self.move_gen_bishop(pos)
-        
+
         elif piece == "Q":
             return self.move_gen_bishop(pos) + self.move_gen_rook(pos)
-        
+
         elif piece == "K":
             return self.move_gen_king(pos)
 
@@ -126,14 +137,14 @@ class game_board():
 
         elif piece == "N":
             return self.move_gen_knight(pos)
-               
+
 
     def move_gen_rook(self, pos: str):
         """
         pos: string (e.g. "A1")\n
-        return a list of legal move (include tile of enemy)       
+        return a list of legal move (include tile of enemy)
         """
-        player = self.game_board[pos][0]        
+        player = self.game_board[pos][0]
         alpha_st = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:self.size]
         moves = []
 
@@ -145,7 +156,7 @@ class game_board():
         for x in alpha_st:
             if x == pos[0]:
                 break
-            
+
             if self.game_board[f"{x}{pos[1]}"] == "·":
                 temp_moves.append(f"{x}{pos[1]}")
 
@@ -159,11 +170,11 @@ class game_board():
         moves += temp_moves[:]
 
         # horzontial from left to right
-        temp_moves = [] 
+        temp_moves = []
         for x in alpha_st[::-1]:
             if x == pos[0]:
                 break
-            
+
             if self.game_board[f"{x}{pos[1]}"] == "·":
                 temp_moves.append(f"{x}{pos[1]}")
 
@@ -199,11 +210,11 @@ class game_board():
         moves += temp_moves[:]
 
         # vertical from top to bottom
-        temp_moves = [] 
+        temp_moves = []
         for y in map(str, range(self.size, 0, -1)):
             if y == pos[1]:
                 break
-            
+
             if self.game_board[f"{pos[0]}{y}"] == "·":
                 temp_moves.append(f"{pos[0]}{y}")
 
@@ -220,11 +231,11 @@ class game_board():
     def move_gen_bishop(self, pos: str):
         """
         pos: string (e.g. "A1")\n
-        return a list of legal move (include tile of enemy)       
+        return a list of legal move (include tile of enemy)
         """
-        player = self.game_board[pos][0]        
+        player = self.game_board[pos][0]
         alpha_st = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:self.size]
-        moves = []        
+        moves = []
 
         # left and down 🡗, start from pos
         # if tile is a piece:
@@ -305,21 +316,21 @@ class game_board():
                 break
             pos0_index += 1
             pos1_index += 1
-        moves += temp_moves[:] 
+        moves += temp_moves[:]
 
         return moves
-            
+
     def move_gen_king(self, pos: str):
         """
         pos: string (e.g. "A1")\n
-        return a list of legal move (include tile of enemy)       
-        """       
+        return a list of legal move (include tile of enemy)
+        """
 
         inside_board = lambda pos0, pos1: (pos0>=0) and (pos0<self.size) and (pos1>=1) and (pos1<=self.size)
 
-        player = self.game_board[pos][0]        
+        player = self.game_board[pos][0]
         alpha_st = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:self.size]
-        moves = []   
+        moves = []
 
         # left and down 🡗, start from pos
         # if tile is a piece:
@@ -333,13 +344,13 @@ class game_board():
         # print(f"POS: {alpha_st[pos0_index]}{pos1_index}")
         if inside_board(pos0_index, pos1_index) and self.game_board[f"{alpha_st[pos0_index]}{pos1_index}"][0] != player:
             moves.append(f"{alpha_st[pos0_index]}{pos1_index}")
-        
+
         # 🡔
         pos1_index += 1
         # print(f"POS: {alpha_st[pos0_index]}{pos1_index}")
         if inside_board(pos0_index, pos1_index) and self.game_board[f"{alpha_st[pos0_index]}{pos1_index}"][0] != player:
             moves.append(f"{alpha_st[pos0_index]}{pos1_index}")
-          
+
         # 🡑
         pos0_index += 1
         # print(f"POS: {alpha_st[pos0_index]}{pos1_index}")
@@ -382,16 +393,16 @@ class game_board():
         """
         Blue pawn can only move up, vice versa\n
         pos: string (e.g. "A1")\n
-        return a list of legal move (include tile of enemy)       
+        return a list of legal move (include tile of enemy)
         """
         # if in OG position, can move forward twice
 
         inside_board = lambda pos0, pos1: (pos0>=0) and (pos0<self.size) and (pos1>=1) and (pos1<=self.size)
 
-        player = self.game_board[pos][0]        
+        player = self.game_board[pos][0]
         alpha_st = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:self.size]
-        moves = []   
-        
+        moves = []
+
 
         # if tile is a piece:
         # same player -> pop all element
@@ -426,15 +437,15 @@ class game_board():
     def move_gen_knight(self, pos: str):
         """
         pos: string (e.g. "A1")\n
-        return a list of legal move (include tile of enemy)       
+        return a list of legal move (include tile of enemy)
         """
 
         inside_board = lambda pos0, pos1: (pos0>=0) and (pos0<self.size) and (pos1>=1) and (pos1<=self.size)
 
-        player = self.game_board[pos][0]        
+        player = self.game_board[pos][0]
         alpha_st = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:self.size]
-        moves = []   
-        
+        moves = []
+
 
         # if tile is a piece:
         # same player -> pop all element
@@ -452,7 +463,7 @@ class game_board():
         pos0_index +=1
         pos1_index +=1
         if inside_board(pos0_index, pos1_index) and self.game_board[f"{alpha_st[pos0_index]}{pos1_index}"][0] != player:
-            moves.append(f"{alpha_st[pos0_index]}{pos1_index}")       
+            moves.append(f"{alpha_st[pos0_index]}{pos1_index}")
 
         # 🡕 2 move
         pos0_index +=2
@@ -478,36 +489,45 @@ class game_board():
         pos0_index-=2
         if inside_board(pos0_index, pos1_index) and self.game_board[f"{alpha_st[pos0_index]}{pos1_index}"][0] != player:
             moves.append(f"{alpha_st[pos0_index]}{pos1_index}")
-        
+
         pos0_index -=1
         pos1_index +=1
         if inside_board(pos0_index, pos1_index) and self.game_board[f"{alpha_st[pos0_index]}{pos1_index}"][0] != player:
             moves.append(f"{alpha_st[pos0_index]}{pos1_index}")
-        
+
 
 
 
         return moves
-    
+
 
     def move_gen_castling(self, pos: str):
         """
         TODO: detect move is legal for castling\n
         pos: string (e.g. "A1")\n
-        return a list of legal move (include tile of enemy)       
+        return a list of legal move (include tile of enemy)
         """
         # king cannot be moves
         # rook cannot have moved
         # king cannot be checked
-        # king cannot moved pass potential check/attack grid 
+        # king cannot moved pass potential check/attack grid
         pass
 
 
-    def undo(self):
+    def undo(self, turn: int):
         """
-        TODO: save perious state of gameboard and allow player to go back 
+        Return perious state of gameboard and allow player to go back
+        turn: the number of turn player want to go back must < self.cache_size
+        return 0
         """
-        pass
+        # this can be use when player make a checkmate move
+
+        if turn > self.cache_size:
+            return 0
+
+        self.game_board = self.game_board_cache[-turn].copy
+        self.game_board_cache = self.game_board_cache[:-turn].copy
+        return 0
 
 
     def print_board_moves_visualize(self, moves: list):
@@ -524,12 +544,12 @@ class game_board():
             else:
                 temp_game_board[pos] += "x"
         alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        
+
         alpha_st = " ".join(alphas[:self.size])
         click.secho(alpha_st.center(self.size*3), fg="bright_white")
         click.echo()
 
-        for i in range(self.size, 0, -1):            
+        for i in range(self.size, 0, -1):
 
             click.secho(f'{i}'.ljust(4), fg="bright_white", nl = False)
 
@@ -561,32 +581,72 @@ class game_board():
         click.secho(alpha_st.center(self.size*3), fg="bright_white")
         return 0
 
+    def is_checkmate(self, piece:list = ["RK", "BK"] ):
+        """
+        piece: list of piece names, can also work with non king pieces
+        return a list of pos: [["check piece pos","king pos"],["check piece pos","king pos"]]
+        """
+        # loop over every space and use move_gen to see whether RK or BK is in the list
+        # if so return the move
+
+        check_pos = []
+
+        for k in self.game_board.keys():
+
+            for m in self.move_generator(k):
+                # click.secho(f"{self.game_board[k]} -> {m}")
+                if self.game_board[m] in piece:
+                    click.secho(f"{self.game_board[k]} is checking {self.game_board[m]}")
+                    check_pos.append([f"{k}", f"{m}"])
+
+        return check_pos
+
+
 
 def main():
-    click.clear()
+    # click.clear()
     game = game_board(8)
     # click.pause()
-    game.game_board["B8"] = 'RK' #not recommended, use fill_pieces_on_board function instead
+    game.game_board["A8"] = 'RK' #not recommended, use fill_pieces_on_board function instead
     game.game_board["G2"] = 'BK'
+    game.game_board["E4"] = 'RB'
 
     #Blue at bottom, red on top
-    game.fill_pieces_on_board({"BQ": ["C6"], "RR": ["C4", "H8"]})
-    click.secho("-----------------------")
-    game.print_board() 
-    
-    click.secho("-----------------------")
-    game.print_board_moves_visualize(game.move_generator("C6"))
-    game.move_piece("C6", "C4")
-
+    game.fill_pieces_on_board({"BQ": ["C6"], "RR": ["C4", "G8"]})
     click.secho("-----------------------")
     game.print_board()
+
+    game.move_piece("C6", "A8")
+    game.move_piece("A8", "A7")
+    game.move_piece("A7", "A1")
+    game.move_piece("A1", "H8")
+    game.move_piece("H8", "H1")
+
+    for m in game.game_board_cache:
+        game.game_board = m
+        game.print_board()
+
+
+    # click.secho("-----------------------")
+    # check_ls = game.is_checkmate()
+    # for m in check_ls:
+    #     click.secho("-----------------------")
+    #     game.print_board_moves_visualize(game.move_generator(f"{m[0]}"))
+
+
+
+    # game.print_board_moves_visualize(game.move_generator("G8"))
+    # game.move_piece("C6", "C4")
+    # click.secho("-----------------------")
+    # game.print_board()
+
     # game.print_board()
 
     # print(game.game_board)
     # click.clear()
-    
+
 
 
 if __name__ == "__main__":
     main()
-    
+
